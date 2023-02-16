b1f1ab22ec28f46cfd7324d1944fc0e945ae9768
Rikki007 <Oreshaodnoglazy@gmail.com>
2023-02-16 12:13:41 +0300

    brackets finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const elementsPair = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    const openingElement = bracketsConfig[i][0];
+    const closingElement = bracketsConfig[i][1];
+    elementsPair[closingElement] = openingElement;
+  }
+
+  const openElement = [];
+  const sameElement = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    const openElem = bracketsConfig[i][0];
+    const closeElem = bracketsConfig[i][1];
+    if (openElem === closeElem) {
+      sameElement.push(openElem);
+    } else {
+      openElement.push(openElem);
+    }
+  }
+
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+
+    if (openElement.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else if (sameElement.includes(currentSymbol)) {
+      if (stack.length > 0 && stack[stack.length - 1] === currentSymbol) {
+        stack.pop();
+      } else {
+        stack.push(currentSymbol);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topStackElement = stack[stack.length - 1];
+      if (elementsPair[currentSymbol] === topStackElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
